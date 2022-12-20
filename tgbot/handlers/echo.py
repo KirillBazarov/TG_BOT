4a6deec66cc43cc972b42f34957b4cd5fe906c7e
demka <kirillbazaroff@gmail.com>
2022-12-20 20:33:19 +0300

    изменил callback_data, доделал весь первый тип фондов

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -2,9 +2,11 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 from aiogram.utils.markdown import hcode
-from tgbot_template.tgbot.keyboards.inline import ikb_funds, ikb_first_type_funds, ikb_second_type_funds, \
-    ikb_fist_type_first_fund, ikb_fist_type_second_fund, ikb_second_type_first_fund, ikb_second_type_second_fund, \
-    ikb_third_type_funds
+from tgbot_template.tgbot.keyboards.inlinelol.ikb_first_funds import ikb_first_type_funds, ikb_fist_type_first_fund, \
+    ikb_fist_type_second_fund, ikb_fist_type_third_fund, ikb_fist_type_fourth_fund, ikb_fist_type_fifth_fund, \
+    ikb_fist_type_sixth_fund, ikb_fist_type_seventh_fund, ikb_fist_type_eighth_fund
+from tgbot_template.tgbot.keyboards.inline import ikb_funds, ikb_second_type_funds, \
+    ikb_second_type_first_fund, ikb_second_type_second_fund, ikb_third_type_funds
 from tgbot_template.tgbot.media.data import funsd
 
 
@@ -14,24 +16,30 @@ async def choose_fund_type(message: types.Message):
     ]
     await message.answer('\n'.join(text), reply_markup=ikb_funds)
 
+
 async def choose_fund_fist_type(call: types.CallbackQuery):
     text = [
         f'так ты выбрал направление {funsd.get("first_fund_type").get("type_name")}, теперь выбирем сам фонд',
     ]
-    await call.message.answer_photo(photo=funsd.get("first_fund_type").get("photo"),reply_markup=ikb_first_type_funds,caption='\n'.join(text))
+    await call.message.answer_photo(photo=funsd.get("first_fund_type").get("photo"), reply_markup=ikb_first_type_funds,
+                                    caption='\n'.join(text))
 
 
 async def choose_fund_second_type(call: types.CallbackQuery):
     text = [
         f'так ты выбрал направлению {funsd.get("second_fund_type").get("type_name")}, теперь выбирем сам фонд',
     ]
-    await call.message.answer_photo(photo=funsd.get("second_fund_type").get("photo"),reply_markup=ikb_second_type_funds,caption='\n'.join(text))
+    await call.message.answer_photo(photo=funsd.get("second_fund_type").get("photo"),
+                                    reply_markup=ikb_second_type_funds, caption='\n'.join(text))
+
 
 async def choose_fund_third_type(call: types.CallbackQuery):
     text = [
         f'так ты выбрал направление {funsd.get("third_fund_type").get("type_name")}, теперь выбирем сам фонд',
     ]
-    await call.message.answer_photo(photo=funsd.get("third_fund_type").get("photo"),reply_markup=ikb_third_type_funds,caption='\n'.join(text))
+    await call.message.answer_photo(photo=funsd.get("third_fund_type").get("photo"), reply_markup=ikb_third_type_funds,
+                                    caption='\n'.join(text))
+
 
 async def fist_type_first_fund(call: types.CallbackQuery):
     text = funsd.get("first_fund_type").get("first_fund").get("info")
@@ -43,12 +51,39 @@ async def fist_type_second_fund(call: types.CallbackQuery):
     await call.message.answer(text=text, reply_markup=ikb_fist_type_second_fund)
 
 
+async def fist_type_third_fund(call: types.CallbackQuery):
+    text = funsd.get("first_fund_type").get("third_fund").get("info")
+    await call.message.answer(text=text, reply_markup=ikb_fist_type_third_fund)
+
+
+async def fist_type_fourth_fund(call: types.CallbackQuery):
+    text = funsd.get("first_fund_type").get("fourth_fund").get("info")
+    await call.message.answer(text=text, reply_markup=ikb_fist_type_fourth_fund)
+
+
+async def fist_type_fifth_fund(call: types.CallbackQuery):
+    text = funsd.get("first_fund_type").get("fifth_fund").get("info")
+    await call.message.answer(text=text, reply_markup=ikb_fist_type_fifth_fund)
+
+async def fist_type_sixth_fund(call: types.CallbackQuery):
+    text = funsd.get("first_fund_type").get("sixth_fund").get("info")
+    await call.message.answer(text=text, reply_markup=ikb_fist_type_sixth_fund)
+
+async def fist_type_seventh_fund(call: types.CallbackQuery):
+    text = funsd.get("first_fund_type").get("seventh_fund").get("info")
+    await call.message.answer(text=text, reply_markup=ikb_fist_type_seventh_fund)
+
+async def fist_type_eighth_fund(call: types.CallbackQuery):
+    text = funsd.get("first_fund_type").get("eighth_fund").get("info")
+    await call.message.answer(text=text, reply_markup=ikb_fist_type_eighth_fund)
+
 async def second_type_first_fund(call: types.CallbackQuery):
     text = [
         f'ты выбрал фонд {funsd.get("second_fund_type").get("first_fund").get("name")}" инфа по нему'
     ]
 
     await call.message.answer('\n'.join(text), reply_markup=ikb_second_type_first_fund)
+
 
 async def second_type_second_fund(call: types.CallbackQuery):
     text = [
@@ -67,22 +102,27 @@ async def delete(call: types.CallbackQuery):
 #     await bot.send_message(message.chat_id, "sup")
 
 
-
 def register_echo(dp: Dispatcher):
     dp.register_message_handler(choose_fund_type, commands="go")
 
     dp.register_callback_query_handler(choose_fund_fist_type, text=funsd.get("first_fund_type").get("type_name"))
-    dp.register_callback_query_handler(fist_type_first_fund, text=funsd.get("first_fund_type").get("first_fund").get("name"))
-    dp.register_callback_query_handler(fist_type_second_fund, text=funsd.get("first_fund_type").get("second_fund").get("name"))
-
+    dp.register_callback_query_handler(fist_type_first_fund, text="first_fund_type:first_fund")
+    dp.register_callback_query_handler(fist_type_second_fund, text="first_fund_type:second_fund")
+    dp.register_callback_query_handler(fist_type_third_fund, text="first_fund_type:third_fund")
+    dp.register_callback_query_handler(fist_type_fourth_fund, text="first_fund_type:fourth_fund")
+    dp.register_callback_query_handler(fist_type_fifth_fund, text="first_fund_type:fifth_fund")
+    dp.register_callback_query_handler(fist_type_sixth_fund, text="first_fund_type:sixth_fund")
+    dp.register_callback_query_handler(fist_type_seventh_fund, text="first_fund_type:seventh_fund")
+    dp.register_callback_query_handler(fist_type_eighth_fund, text="first_fund_type:eighth_fund")
 
     dp.register_callback_query_handler(choose_fund_second_type, text=funsd.get("second_fund_type").get("type_name"))
-    dp.register_callback_query_handler(second_type_first_fund, text=funsd.get("second_fund_type").get("first_fund").get("name"))
-    dp.register_callback_query_handler(second_type_second_fund, text=funsd.get("second_fund_type").get("second_fund").get("name"))
+    dp.register_callback_query_handler(second_type_first_fund,
+                                       text=funsd.get("second_fund_type").get("first_fund").get("name"))
+    dp.register_callback_query_handler(second_type_second_fund,
+                                       text=funsd.get("second_fund_type").get("second_fund").get("name"))
 
     dp.register_callback_query_handler(choose_fund_third_type, text=funsd.get("third_fund_type").get("type_name"))
 
     dp.register_callback_query_handler(choose_fund_third_type, text=funsd.get("third_fund_type").get("type_name"))
 
-
-    dp.register_callback_query_handler(delete, text="stepback")
+    dp.register_callback_query_handler(delete, text="обратно")
--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -1,12 +1,15 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
 
 from tgbot_template.tgbot.media.data import funsd
+
+stepback_button = InlineKeyboardButton(text="Обратно", callback_data="обратно")
 
 ikb_funds = InlineKeyboardMarkup(row_width=2,
                                  inline_keyboard=[[
 
                                      InlineKeyboardButton(text=funsd.get("first_fund_type").get("type_name"),
-                                                          callback_data=funsd.get("first_fund_type").get(
+                                                            callback_data=funsd.get("first_fund_type").get(
                                                               "type_name")),
                                      InlineKeyboardButton(funsd.get("second_fund_type").get("type_name"),
                                                           callback_data=funsd.get("second_fund_type").get(
@@ -22,61 +25,6 @@ ikb_funds = InlineKeyboardMarkup(row_width=2,
 
                                  ]])
 
-ikb_first_type_funds = InlineKeyboardMarkup(row_width=2,
-                                            inline_keyboard=[
-                                                [
-                                                    InlineKeyboardButton(
-                                                        text=funsd.get("first_fund_type").get("first_fund").get("name"),
-                                                        callback_data=funsd.get("first_fund_type").get(
-                                                            "first_fund").get("name")),
-                                                    InlineKeyboardButton(
-                                                        text=(funsd.get("first_fund_type").get("second_fund").get(
-                                                            "name")),
-                                                        callback_data=(
-                                                            funsd.get("first_fund_type").get("second_fund").get(
-                                                                "name"))),
-                                                ],
-                                                [
-                                                    InlineKeyboardButton(
-                                                        text=funsd.get("first_fund_type").get("third_fund").get("name"),
-                                                        callback_data=funsd.get("first_fund_type").get(
-                                                            "third_fund").get("name")),
-                                                    InlineKeyboardButton(
-                                                        text=(funsd.get("first_fund_type").get("fourth_fund").get(
-                                                            "name")),
-                                                        callback_data=(
-                                                            funsd.get("first_fund_type").get("fourth_fund").get(
-                                                                "name"))),
-                                                ],
-                                                    [
-                                                    InlineKeyboardButton(
-                                                        text=funsd.get("first_fund_type").get("fifth_fund").get("name"),
-                                                        callback_data=funsd.get("first_fund_type").get(
-                                                            "fifth_fund").get("name")),
-                                                    InlineKeyboardButton(
-                                                        text=(funsd.get("first_fund_type").get("sixth_fund").get(
-                                                            "name")),
-                                                        callback_data=(
-                                                            funsd.get("first_fund_type").get("sixth_fund").get(
-                                                                "name"))),
-                                                ],
-[
-                                                    InlineKeyboardButton(
-                                                        text=funsd.get("first_fund_type").get("seventh_fund").get("name"),
-                                                        callback_data=funsd.get("first_fund_type").get(
-                                                            "seventh_fund").get("name")),
-                                                    InlineKeyboardButton(
-                                                        text=(funsd.get("first_fund_type").get("eighth_fund").get(
-                                                            "name")),
-                                                        callback_data=(
-                                                            funsd.get("first_fund_type").get("eighth_fund").get(
-                                                                "name"))),
-                                                ],
-                                                [
-                                                    InlineKeyboardButton(text="Обратно",
-                                                                         callback_data="stepback"),
-                                                ]
-                                            ])
 
 ikb_second_type_funds = InlineKeyboardMarkup(row_width=2,
                                             inline_keyboard=[
@@ -106,8 +54,7 @@ ikb_second_type_funds = InlineKeyboardMarkup(row_width=2,
                                                 ],
 
                                                 [
-                                                    InlineKeyboardButton(text="Обратно",
-                                                                         callback_data="stepback"),
+                                                    stepback_button,
                                                 ]
                                             ])
 
@@ -119,8 +66,7 @@ ikb_third_type_funds = InlineKeyboardMarkup(row_width=2,
                                                         callback_data=funsd.get("third_fund_type").get("first_fund").get("name")),
                                                 ],
                                                 [
-                                                    InlineKeyboardButton(text="Обратно",
-                                                                         callback_data="stepback"),
+                                                    stepback_button,
                                                 ]]
                                             )
 # ikb_fourth_type_funds = InlineKeyboardMarkup(row_width=2,
@@ -146,36 +92,6 @@ ikb_third_type_funds = InlineKeyboardMarkup(row_width=2,
                                                 #                          callback_data="stepback"),
                                                 # ]])
 
-ikb_fist_type_first_fund = InlineKeyboardMarkup(row_width=2,
-                                                inline_keyboard=[
-                                                    [
-                                                        InlineKeyboardButton(
-                                                            text=funsd.get("first_fund_type").get("first_fund").get(
-                                                                "name"),
-                                                            url=funsd.get("first_fund_type").get("first_fund").get(
-                                                                "site")),
-                                                    ],
-
-                                                    [
-                                                        InlineKeyboardButton(text="Обратно",
-                                                                             callback_data="stepback"),
-                                                    ]
-                                                ])
-
-ikb_fist_type_second_fund = InlineKeyboardMarkup(row_width=2,
-                                                 inline_keyboard=[
-                                                     [
-                                                         InlineKeyboardButton(
-                                                             text=funsd.get("first_fund_type").get("second_fund").get(
-                                                                 "info"),
-                                                             url=funsd.get("first_fund_type").get("second_fund").get(
-                                                                 "site")),
-                                                     ],
-                                                     [
-                                                         InlineKeyboardButton(text="Обратно",
-                                                                              callback_data="stepback"),
-                                                     ]
-                                                 ])
 
 ikb_second_type_first_fund = InlineKeyboardMarkup(row_width=2,
                                                   inline_keyboard=[
@@ -186,8 +102,7 @@ ikb_second_type_first_fund = InlineKeyboardMarkup(row_width=2,
                                                               url=funsd.get("second_fund_type").get("first_fund").get(
                                                                   "site"))],
                                                       [
-                                                          InlineKeyboardButton(text="Обратно",
-                                                                               callback_data="stepback"),
+                                                          stepback_button,
                                                       ]
                                                   ])
 
@@ -200,7 +115,6 @@ ikb_second_type_second_fund = InlineKeyboardMarkup(row_width=2,
                                                                url=funsd.get("second_fund_type").get("second_fund").get(
                                                                    "site"))],
                                                        [
-                                                           InlineKeyboardButton(text="Обратно",
-                                                                                callback_data="stepback"),
+                                                           stepback_button,
                                                        ]
                                                    ])
